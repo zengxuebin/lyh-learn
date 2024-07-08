195d8b0eed5543d128001e5c8a09a51cf4cd4ffe
luoyuhao <leishengluo@gmail.com>
2024-07-08 21:13:54 +0800

    lyh first commit

--- java-se/src/Main.java
+++ java-se/src/Main.java
@@ -1,6 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    
+    // lyh-learn
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
